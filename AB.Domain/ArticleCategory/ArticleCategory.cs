1c968b8cf801f100976ec9777c2cd36e5b7f2d91
arminatwork <workwitharmin7@gmail.com>
2021-03-31 03:38:09 +0430

    Add ArticleCategory into AB.Domain

---
+++ AB.Domain/ArticleCategory/ArticleCategory.cs
@@ -0,0 +1,21 @@
+using System;
+namespace AB.Domain.ArticleCategory
+{
+    public class ArticleCategory
+    {
+        public long Id { get; private set; }
+
+        public string Title { get; private set; }
+
+        public bool IsDeleted { get; private set; }
+
+        public DateTime CreationDate { get; private set; }
+
+        public ArticleCategory(string title)
+        {
+            Title = title;
+            IsDeleted = false;
+            CreationDate = DateTime.Now;
+        }
+    }
+}
