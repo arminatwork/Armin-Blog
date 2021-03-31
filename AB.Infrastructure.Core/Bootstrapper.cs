c036da833029bde14b809925ef6a0c78f6c472e3
arminatwork <workwitharmin7@gmail.com>
2021-03-31 19:13:10 +0430

    Add Bootstrapper.cs into AB.Infrastructure.Core

---
+++ AB.Infrastructure.Core/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using AB.Application;
+using AB.Application.Contracts.ArticleCategory;
+using AB.Domain.ArticleCategory;
+using AB.Infrastructure.EfCore;
+using AB.Infrastructure.EfCore.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AB.Infrastructure.Core
+{
+    public static class Bootstrapper
+    {
+        public static void Configure(this IServiceCollection services, string connectionString)
+        {
+            services.AddTransient<IArticleCategoryApplication, ArticleCategoryApplication>();
+            services.AddTransient<IArticleCategoryRepository, ArticleCategoryRepository>();
+            services.AddDbContext<ArminBlogContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+        }
+    }
+}
