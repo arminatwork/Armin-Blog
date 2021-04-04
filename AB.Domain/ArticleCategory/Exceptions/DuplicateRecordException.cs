10fbfdf87d1cd4e09884837bb2bed310dfacaaf3
arminatwork <workwitharmin7@gmail.com>
2021-04-04 19:37:24 +0430

    Add DuplicateRecordException in ArticleCategory Domain

---
+++ AB.Domain/ArticleCategory/Exceptions/DuplicateRecordException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AB.Domain.ArticleCategory.Exceptions
+{
+    public class DuplicateRecordException : Exception
+    {
+        public DuplicateRecordException()
+        {
+            
+        }
+
+        public DuplicateRecordException(string message)
+        {
+            
+        }
+    }
+}
