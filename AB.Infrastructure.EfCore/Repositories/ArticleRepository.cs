5e9c288851856f6ed804841e122289dcf95d48d2
arminatwork <workwitharmin7@gmail.com>
2021-04-04 20:37:02 +0430

    Add ArticleRepository into AB.Infrastructure.EfCore/Repositories

---
+++ AB.Infrastructure.EfCore/Repositories/ArticleRepository.cs
@@ -0,0 +1,14 @@
+﻿using AB.Domain.Article;
+
+namespace AB.Infrastructure.EfCore.Repositories
+{
+    public class ArticleRepository : IArticleRepository
+    {
+        private readonly IArticleRepository _repository;
+
+        public ArticleRepository(IArticleRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
