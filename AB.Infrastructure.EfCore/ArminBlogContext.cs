43e0f7e7cbb1fcfb7fc12bbb89662706bb6987a1
arminatwork <workwitharmin7@gmail.com>
2021-04-04 20:37:34 +0430

    Write Article Domain and Apply ArticleConfiguration

--- AB.Infrastructure.EfCore/ArminBlogContext.cs
+++ AB.Infrastructure.EfCore/ArminBlogContext.cs
@@ -1,3 +1,4 @@
+using AB.Domain.Article;
 using AB.Domain.ArticleCategory;
 using AB.Infrastructure.EfCore.Configurations;
 using Microsoft.EntityFrameworkCore;
@@ -14,12 +15,14 @@ namespace AB.Infrastructure.EfCore
         //Model Creation
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+
             modelBuilder.ApplyConfiguration(new ArticleCategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ArticleConfiguration());
             base.OnModelCreating(modelBuilder);
         }
 
         public DbSet<ArticleCategory> ArticleCategories { get; set; }
+        public DbSet<Article> Articles { get; set; }
 
     }
 }
