f545c4cc59ac828841ca09c506ad2dc9501b8328
arminatwork <workwitharmin7@gmail.com>
2021-04-04 22:12:33 +0430

    Write List Method to Get List of ArticleViewModel

--- AB.Application/ArticleApplication.cs
+++ AB.Application/ArticleApplication.cs
@@ -1,8 +1,22 @@
-﻿using AB.Application.Contracts.Article;
+﻿using System.Collections.Generic;
+using System.Linq;
+using AB.Application.Contracts.Article;
+using AB.Domain.Article;
 
 namespace AB.Application
 {
     public class ArticleApplication : IArticleApplication
     {
+        private readonly IArticleRepository _repository;
+
+        public ArticleApplication(IArticleRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<ArticleViewModel> List()
+        {
+            return _repository.GetAll().ToList();
+        }
     }
 }
