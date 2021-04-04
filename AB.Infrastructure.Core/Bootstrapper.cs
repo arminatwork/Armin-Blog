c533f1864420ffa7fc0ce1c40485c592fa83b7e4
arminatwork <workwitharmin7@gmail.com>
2021-04-04 20:28:06 +0430

    Write Article Dependencies

--- AB.Infrastructure.Core/Bootstrapper.cs
+++ AB.Infrastructure.Core/Bootstrapper.cs
@@ -1,5 +1,7 @@
 ﻿using AB.Application;
+using AB.Application.Contracts.Article;
 using AB.Application.Contracts.ArticleCategory;
+using AB.Domain.Article;
 using AB.Domain.ArticleCategory;
 using AB.Domain.ArticleCategory.Services;
 using AB.Infrastructure.EfCore;
@@ -13,9 +15,17 @@ namespace AB.Infrastructure.Core
     {
         public static void Configure(this IServiceCollection services, string connectionString)
         {
+            //ArticleCategory
             services.AddTransient<IArticleCategoryApplication, ArticleCategoryApplication>();
             services.AddTransient<IArticleCategoryRepository, ArticleCategoryRepository>();
             services.AddTransient<IArticleCategoryValidatorService, ArticleCategoryValidatorService>();
+
+            //Article
+            services.AddTransient<IArticleApplication, ArticleApplication>();
+            services.AddTransient<IArticleRepository, ArticleRepository>();
+
+
+            //Database
             services.AddDbContext<ArminBlogContext>(options =>
             {
                 options.UseSqlServer(connectionString);
